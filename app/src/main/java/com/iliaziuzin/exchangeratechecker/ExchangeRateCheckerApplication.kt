1e70d858b307d8e495d379ca52949b35616b8066
Ilya Zyuzin <beceji9iu@gmail.com>
2026-02-04 19:32:25 +0400

    Initial commit

---
+++ app/src/main/java/com/iliaziuzin/exchangeratechecker/ExchangeRateCheckerApplication.kt
@@ -0,0 +1,7 @@
+package com.iliaziuzin.exchangeratechecker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExchangeRateCheckerApplication : Application()
