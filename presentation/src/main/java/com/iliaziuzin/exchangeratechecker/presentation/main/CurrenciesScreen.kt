6d16adc03fe582c71391d4d649ed23b2ad7a3198
Ilya Zyuzin <beceji9iu@gmail.com>
2026-02-06 22:38:33 +0400

    Dropdown composable moved

--- presentation/src/main/java/com/iliaziuzin/exchangeratechecker/presentation/main/CurrenciesScreen.kt
+++ presentation/src/main/java/com/iliaziuzin/exchangeratechecker/presentation/main/CurrenciesScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -20,13 +19,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +31,6 @@ import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,27 +39,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.layout.positionInWindow
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.fastForEachIndexed
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupPositionProvider
 import com.iliaziuzin.exchangeratechecker.models.UiCurrencyExchangePair
 import com.iliaziuzin.exchangeratechecker.presentation.R
 import com.iliaziuzin.exchangeratechecker.presentation.main.composable.CurrencyComposable
+import com.iliaziuzin.exchangeratechecker.presentation.main.composable.PositionedDropdownMenu
 import com.iliaziuzin.exchangeratechecker.ui.theme.BackgroundDefault
 import com.iliaziuzin.exchangeratechecker.ui.theme.BackgroundHeader
 import com.iliaziuzin.exchangeratechecker.ui.theme.Outline
@@ -246,7 +235,6 @@ fun CurrenciesHeader(modifier: Modifier = Modifier,
                         )
                     }
                 }
-                Spacer(modifier = Modifier.size(16.dp))
                 Spacer(modifier = Modifier
                     .fillMaxWidth()
                     .size(1.dp)
@@ -254,48 +242,6 @@ fun CurrenciesHeader(modifier: Modifier = Modifier,
             }
 
         }
-}
-
-@Composable
-private fun PositionedDropdownMenu(
-    expanded: Boolean,
-    onDismissRequest: () -> Unit,
-    anchorSize: IntSize,
-    anchorPosition: IntOffset,
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit,
-) {
-    val popupPositionProvider = object : PopupPositionProvider {
-        override fun calculatePosition(
-            anchorBounds: IntRect,
-            windowSize: IntSize,
-            layoutDirection: LayoutDirection,
-            popupContentSize: IntSize
-        ): IntOffset {
-            return IntOffset(
-                x = anchorPosition.x,
-                y = anchorPosition.y
-            )
-        }
-    }
-
-    if (expanded) {
-        Popup(
-            popupPositionProvider = popupPositionProvider,
-            onDismissRequest = onDismissRequest
-        ) {
-            Surface(
-                shape = RoundedCornerShape(8.dp),
-                border = BorderStroke(1.dp, MaterialTheme.colorScheme.Secondary),
-                modifier = modifier
-                    .width(with(LocalDensity.current) { anchorSize.width.toDp() })
-                    .heightIn(max = 216.dp)
-                    .background(MaterialTheme.colorScheme.BackgroundHeader)
-            ) {
-                content()
-            }
-        }
-    }
 }
 
 @Preview(showBackground = true)
